f501bddf9ecfb73be9fe56003c3fb2ab59cebb2c
Ferhuzje <86949390+Ferhuzje@users.noreply.github.com>
2021-09-08 12:05:43 +0200

    implements Serializable

--- src/comcave/Kunde.java
+++ src/comcave/Kunde.java
@@ -1,8 +1,9 @@
 package comcave;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Kunde {
+public class Kunde implements Serializable {
 	
 	public static ArrayList<Kunde> kundenListe = new ArrayList<Kunde>();
 	
